cb70a624d8a683a4aab713fb022208b44345ce4a
Christina Lidwin <clidwin@gmail.com>
2014-08-03 22:41:37 -0700

    Site Version 1.2.0
    - Added About Page
    - Added Contact Page
    - Updated Javascript
    - Improved Responsiveness

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -94,4 +94,22 @@ $( document ).ready( function() {
     $('#contact').submit(validateAndSendForm);
     
     // Page smooth scroll: http://stackoverflow.com/questions/4198041/jquery-smooth-scroll-to-an-anchor
+    var hashTagActive = "";
+    $(".nav").click(function (event) {
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 1000, 'swing');
+            hashTagActive = this.hash;
+        }
+    });
 });
