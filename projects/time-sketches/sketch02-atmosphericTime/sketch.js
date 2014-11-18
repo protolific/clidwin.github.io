aa02211dbd2fc83ec81efecd1d69453823cea2f9
Christina Lidwin <clidwin@gmail.com>
2014-11-18 01:00:51 -0500

    Version 1.5.5
    Updated the atmospheric sketch to utilize the weather data and
    pVectors. Also assigned color based on temperature and created two
    gradient options.

--- projects/time-sketches/sketch02-atmosphericTime/sketch.js
+++ projects/time-sketches/sketch02-atmosphericTime/sketch.js
@@ -5,10 +5,10 @@
  * Author: Christina Lidwin
  * 
  * Created On: October 20, 2014
- * Modified On: November 13, 2014
+ * Modified On: November 18, 2014
  * 
  * Design Goal: To create a visual piece where the rate at which objects moves
- *    relates to the wind speed, color relates to the temperature, etc.
+ *    relates to the wind speed/direction, color relates to the temperature, etc.
  * 
  * Inspiration: The place where I work has no windows that face the outdoors,
  *    and as a result, we were thinking of ways to bring information about the
@@ -19,36 +19,35 @@
  * Interactivity: None specified (yet). Maybe allow the capability to enter in
  *    a custom city/state.
  * 
- * TODOS: Connect motion, color, etc to weather data
+ * TODOS: Connect motion to weather data, auto-update visualization every 15min
  */
- 
+
+// Visual Elements
+var triangles;
+var hue;
+
+// Weather Elements
 var weather_city = 'Blacksburg';
 var weather_state = 'VA';
 var temp_f;
 var wind_mph;
 var wind_degrees;
-var triangles;
 
 /**
  * Custom triangle class.
+ * @param position The first point in the triangle.
+ * @param velocity The velocity of the triangle.
+ * @param velocity The amount of rotation to be applied each frame.
  */
-var Triangle = function(point1X, point1Y, velocityX, velocityY) {
-  //TODO(clidwin): Translate these points into PVectors
-  this.point1X = point1X;
-  this.point1Y = point1Y;
-  this.point2X = random(25, 100);
-  this.point2Y = random(25, 100);
-  this.point3X = random(25, 100);
-  this.point3Y = 0 - random(25, 100);
+var Triangle = function(position, velocity, rotationFactor) {
+  this.point1 = position;
+  this.point2 = createVector(random(25, 100), random(25, 100));
+  this.point3 = createVector(random(25, 100), 0 - random(25, 100));
   
   this.saturation = random(15, 100);
   
-  //TODO(clidwin): Use PVectors and make Velocity an argument
-  this.velocityX = velocityX;
-  this.velocityY = velocityY;
-  
-  //TODO(clidwin): Make rotation an argument.
-  this.rotationFactor = random(-5, 5);
+  this.velocity = velocity;
+  this.rotationFactor = rotationFactor;
   this.rotation = this.rotationFactor;
 }
 
@@ -66,41 +65,87 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   background(350, 100, 40);
   
-  // Get initial weather data
-  //getData();
   createTriangles();
+  parseWeatherData();
 }
 
 /**
  * Operations that occur once per frame rendering
  */
 function draw() {
-  background(35, 20, 100);
-  //TODO(clidwin): draw a gradient
-  //Linear gradient: https://processing.org/examples/lineargradient.html
-  //Radial gradient: https://www.processing.org/examples/radialgradient.html
+  background(hue, 100, 90);
+
+  //draw a gradient
+  //drawLinearGradient()
+  drawRadialGradient(width/2, height/2);
   
-  /*textSize(28);
+  /*fill(0);
+  textSize(28);
   text(wind_mph, 0, 20);
   text(wind_degrees, 0, 80);*/
   
-  stroke(0,0,0);
-  strokeWeight(2);
+  // Applying a stroke for the triangles.
+  stroke(350,0,0, 75);
+  strokeWeight(1);
+  noStroke();
   
   // Draw all of the triangles on the canvas
   for (var i=0; i<triangles.length; i++) {
-    fill(35, triangles[i].saturation, 100);
+    fill(hue, triangles[i].saturation, 90);
     
     push();
-    translate(triangles[i].point1X, triangles[i].point1Y);
+    // Apply transforms.
+    translate(triangles[i].point1.x, triangles[i].point1.y);
     rotate(triangles[i].rotation);
+    
+    // Draw triangle. TODO(clidwin): make this a function of the triangle class
     triangle(
       0, 0, // First point of triangle
-      triangles[i].point2X, triangles[i].point2Y, // Second point of triangle
-      triangles[i].point3X, triangles[i].point3Y); // Third point of triangle
+      triangles[i].point2.x, triangles[i].point2.y, // Second point of triangle
+      triangles[i].point3.x, triangles[i].point3.y); // Third point of triangle
     pop();
     
     moveTriangle(triangles[i]);
+  }
+}
+
+/**
+ * Draws a radial gradient.
+ * Based on: https://www.processing.org/examples/radialgradient.html
+ */
+function drawRadialGradient() {
+  noStroke();
+  var dim = width;
+  var radius = dim;
+  var s = 100;
+  var x = width/2;
+  var y = height/2;
+  for (var r = radius; r > 0; r-=5) {
+    fill(hue, s, 90);
+    ellipse(x, y, r, r);
+    s-=0.75;
+  }
+}
+
+/**
+ * Draws a linear gradient.
+ * Based on: https://processing.org/examples/lineargradient.html
+ */
+function drawLinearGradient() {
+  var x = 0;
+  var y = 0;
+  var w = width;
+  var h = height;
+  
+  var c1 = color(hue, 100, 90);
+  var c2 = color(hue, 100, 90);
+  noFill();
+  for (var i = y; i <= y+h; i++) {
+    //var inter = map(i, y, y+h, 0, 0.01);
+    //var c = lerpColor(c1, c2, 1);
+    s = i*0.15;
+    stroke(hue, s, 90);
+    line(x, i, x+w, i);
   }
 }
 
@@ -110,23 +155,23 @@ function draw() {
  */
 function moveTriangle(triangle) {
   // Handle the case when an X value has gone out of bounds.
-  if (triangle.point1X >= width || triangle.point1X <= 0 ||
-        triangle.point1X >= width || triangle.point1X <= 0 ||
-        triangle.point1X + triangle.point2X >= width || 
-        triangle.point1X + triangle.point2X <= 0 ||
-        triangle.point1X + triangle.point3X >= width || 
-        triangle.point1X + triangle.point3X <= 0) {
-      triangle.velocityX = triangle.velocityX * (-1);
+  if (triangle.point1.x >= width || triangle.point1.x <= 0 ||
+        triangle.point1.x >= width || triangle.point1.x <= 0 ||
+        triangle.point1.x + triangle.point2.x >= width || 
+        triangle.point1.x + triangle.point2.x <= 0 ||
+        triangle.point1.x + triangle.point3.x >= width || 
+        triangle.point1.x + triangle.point3.x <= 0) {
+      triangle.velocity.x = triangle.velocity.x * (-1);
     }
   
   // Handle the case when the Y value has gone out of bounds.
-  if (triangle.point1Y > width || triangle.point1Y <= 0 ||
-      triangle.point1Y >= width || triangle.point1Y <= 0 ||
-      triangle.point1Y + triangle.point2Y >= width || 
-      triangle.point1Y + triangle.point2Y <= 0 ||
-      triangle.point1Y + triangle.point3Y >= width || 
-      triangle.point1Y + triangle.point3Y <= 0) {
-    triangle.velocityY = triangle.velocityY * (-1);
+  if (triangle.point1.y > width || triangle.point1.y <= 0 ||
+      triangle.point1.y >= width || triangle.point1.y <= 0 ||
+      triangle.point1.y + triangle.point2.y >= width || 
+      triangle.point1.y + triangle.point2.y <= 0 ||
+      triangle.point1.y + triangle.point3.y >= width || 
+      triangle.point1.y + triangle.point3.y <= 0) {
+    triangle.velocity.y = triangle.velocity.y * (-1);
   }
     
   // Scale back rotation value when it's larger than 360 degrees.
@@ -135,8 +180,7 @@ function moveTriangle(triangle) {
   }
   
   // Update velocity and rotation.
-  triangle.point1X += triangle.velocityX;
-  triangle.point1Y += triangle.velocityY;
+  triangle.point1.add(triangle.velocity.x, triangle.velocity.y);
   triangle.rotation += triangle.rotationFactor;
 }
 
@@ -145,7 +189,7 @@ function moveTriangle(triangle) {
  * conditions. Response comes in as a JSON packet, so the information is parsed
  * into local variables.
  */
-function getData() {
+function parseWeatherData() {
   var weatherUrl = 
       'http://api.wunderground.com/api/eac387d283139277/geolookup/conditions/q/' 
       + weather_state+ '/' + weather_city + '.json';
@@ -160,6 +204,8 @@ function getData() {
       temp_f = parsed_json['current_observation']['temp_f'];
       wind_degrees = parsed_json['current_observation']['wind_degrees'];
       wind_mph = parsed_json['current_observation']['wind_mph'];
+      
+      setHue();
     },
     error: function(parsed_json) {
       alert(parsed_json);
@@ -168,7 +214,28 @@ function getData() {
 }
 
 /**
- * Creates triangles based on the created points.
+ * Sets the hue color based on the current temperature (farenheit)
+ */
+function setHue() {
+  if (temp_f < 0) {
+    hue = 264; // Violet
+  } else if (temp_f < 17) {
+    hue = 224; // Dark Blue
+  } else if (temp_f < 33) {
+    hue = 208; // Medium Blue
+  } else if (temp_f < 50) {
+    hue = 181; // Light Blue
+  } else if (temp_f < 68) {
+    hue = 131; // Green
+  } else if (temp_f < 86) {
+    hue = 59; // Yellow
+  } else {
+    hue = 8; //Red
+  }
+}
+
+/**
+ * Creates triangles based on a pseudo-random (noise-based) set of points.
  */
 function createTriangles() {
   triangles = [];
@@ -177,13 +244,15 @@ function createTriangles() {
   var yOff = 0;
   
   for (var i=0; i<100; i++) {
-    // Find the first point's x and y values.
-    var xPos = noise(xOff + 2*i) * width;
-    var yPos = noise(yOff - 2*i) * height;
+    // Find the first point's 2D position and velocity.
+    var pos = 
+          createVector(noise(xOff + 2*i) * width, noise(yOff - 2*i) * height);
+    var velocity = createVector(random(-2, 2), random(-2, 2));
+    
+    var rotationFactor = random(-5, 5);
     
     // Create the triangle object.
-    triangles[triangles.length] 
-        = new Triangle(xPos, yPos, random(-2, 2), random(-2, 2));
+    triangles[triangles.length] = new Triangle(pos, velocity, rotationFactor);
     
     // Update the offset values.
     xOff += 20;
