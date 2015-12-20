9aaea61a22dffdaa25aaad9f6b7f0d656515a7dd
Christina Lidwin <clidwin@gmail.com>
2015-12-20 13:38:41 -0800

    Temporary Site Uploaded
    New site is a work in progress, but this one-page landing page keeps
    social media links in tact.

--- projects/web-education/assets/js/basic.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('#module-design-icon').hover(function () {
-        alert('hello');
-        $('#module-design-icon').removeClass('entypo-brush');
-        $('#module-design-icon').addClass('entypo-rocket');
-    });
-});
